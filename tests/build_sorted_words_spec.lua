72c1a9401b7ba1b40a9a8391de7ad6167cc9a67b
javierpoduje <javier.poduje@gmail.com>
2025-01-13 19:55:58 +0100

    disable diagnostics on tests

--- tests/build_sorted_words_spec.lua
+++ tests/build_sorted_words_spec.lua
@@ -1,41 +1,53 @@
 local build_sorted_words = require('smartsort')._build_sorted_words
 
+--- @diagnostic disable-next-line: undefined-global
 describe("smartsort._build_sorted_words", function()
+    --- @diagnostic disable-next-line: undefined-global
     it("should error if 'spaces_between_words' has a length other than 'words' - 1", function()
+        --- @diagnostic disable-next-line: undefined-field
         assert.has_error(function()
             build_sorted_words({ 1, 1 }, { true }, { "test", "deez" })
         end)
 
+        --- @diagnostic disable-next-line: undefined-field
         assert.has_error(function()
             build_sorted_words({ 3 }, { true }, { "what", "about", "deez", "testies" })
         end)
 
+        --- @diagnostic disable-next-line: undefined-field
         assert.has_error(function()
             build_sorted_words({ 1, 1, 1, 1, 1, }, { true }, { "this", "is", "a", "test" })
         end)
     end)
 
+    --- @diagnostic disable-next-line: undefined-global
     it("should error if 'words_with_end_comma' has a length other than 'words'", function()
+        --- @diagnostic disable-next-line: undefined-field
         assert.has_error(function()
             build_sorted_words({ 1, 1, 1 }, { true }, { "this's", "a", "test" })
         end)
 
+        --- @diagnostic disable-next-line: undefined-field
         assert.has_error(function()
             build_sorted_words({ 1, 1, 1 }, { true, false, true, false }, { "this's", "a", "test" })
         end)
     end)
 
+    --- @diagnostic disable-next-line: undefined-global
     it("should add comma only to words with comma at the end", function()
         local spaces_between_words = { 1, 1 }
         local words_with_end_comma = { true, false, true }
         local words = { "this", "is", "a" }
+        --- @diagnostic disable-next-line: undefined-field
         assert.are.equals("this, is a,", build_sorted_words(spaces_between_words, words_with_end_comma, words))
     end)
 
+    --- @diagnostic disable-next-line: undefined-global
     it("should respect spaces between words", function()
         local spaces_between_words = { 1, 2, 3 }
         local words_with_end_comma = { true, false, true, false }
         local words = { "this", "is", "a", "test" }
+        --- @diagnostic disable-next-line: undefined-field
         assert.are.equals("this, is  a,   test", build_sorted_words(spaces_between_words, words_with_end_comma, words))
     end)
 end)
--- tests/calculate_spaces_between_words_spec.lua
+++ tests/calculate_spaces_between_words_spec.lua
@@ -1,21 +1,32 @@
 local calculate_spaces_between_words = require('smartsort')._calculate_spaces_between_words
 
+--- @diagnostic disable-next-line: undefined-global
 describe("smartsort._calculate_spaces_between_words", function()
+    --- @diagnostic disable-next-line: undefined-global
     it("should return an empty list if an string with one word or less is given", function()
+        --- @diagnostic disable-next-line: undefined-field
         assert.are.same({}, calculate_spaces_between_words(""))
+        --- @diagnostic disable-next-line: undefined-field
         assert.are.same({}, calculate_spaces_between_words("thisisatest"))
     end)
 
+    --- @diagnostic disable-next-line: undefined-global
     it("should return one item per word - 1", function()
+        --- @diagnostic disable-next-line: undefined-field
         assert.are.same({ 1 }, calculate_spaces_between_words("test deez"))
+        --- @diagnostic disable-next-line: undefined-field
         assert.are.same({ 1, 1, 1 }, calculate_spaces_between_words("this is a test"))
     end)
 
+    --- @diagnostic disable-next-line: undefined-global
     it("should detect more than one space", function()
+        --- @diagnostic disable-next-line: undefined-field
         assert.are.same({ 3 }, calculate_spaces_between_words("test   deez"))
     end)
 
+    --- @diagnostic disable-next-line: undefined-global
     it("should detect inconsistent spacing", function()
+        --- @diagnostic disable-next-line: undefined-field
         assert.are.same({ 3, 1, 2 }, calculate_spaces_between_words("test   deez words,  bruh"))
     end)
 end)
