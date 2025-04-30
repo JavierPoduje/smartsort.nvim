a5d841e0fd54c01912189f9d76b5c59bad20e2b6
javierpoduje <javier.poduje@gmail.com>
2025-04-30 20:10:07 +0200

    fix: ci tests

--- scripts/minimal_init.vim
+++ scripts/minimal_init.vim
@@ -20,6 +20,9 @@ end, required_parsers)
 if #to_install > 0 then
   -- fixes 'pos_delta >= 0' error - https://github.com/nvim-lua/plenary.nvim/issues/52
   vim.cmd('set display=lastline')
+  vim.cmd('set splitbelow=false')
+  vim.cmd('set splitright=false')
+
   -- make "TSInstall*" available
   vim.cmd 'runtime! plugin/nvim-treesitter.vim'
   vim.cmd('TSInstallSync ' .. table.concat(to_install, ' '))
--- tests/build_sorted_words_spec.lua
+++ tests/build_sorted_words_spec.lua
@@ -11,22 +11,22 @@ local has_error = assert.has_error
 
 describe("smartsort._build_sorted_words", function()
     it("should error if 'spaces_between_words' has a length other than 'words' - 1", function()
-        has_error(function() build_sorted_words({ 1, 1 }, { "test", "deez" }) end)
-        has_error(function() build_sorted_words({ 3 }, { "what", "about", "deez", "testies" }) end)
-        has_error(function() build_sorted_words({ 1, 1, 1, 1, 1, }, { "this", "is", "a", "test" }) end)
+        has_error(function() build_sorted_words({ 1, 1 }, { "test", "deez" }, ",") end)
+        has_error(function() build_sorted_words({ 3 }, { "what", "about", "deez", "testies" }, ",") end)
+        has_error(function() build_sorted_words({ 1, 1, 1, 1, 1, }, { "this", "is", "a", "test" }, ",") end)
     end)
 
     it("should respect spaces between words", function()
         equals(
             "this, is,  a,   test",
-            build_sorted_words({ 1, 2, 3 }, { "this", "is", "a", "test" })
+            build_sorted_words({ 1, 2, 3 }, { "this", "is", "a", "test" }, ",")
         )
     end)
 
     it("should respect non-alphanumeric characters", function()
         equals(
             '"c", "ss", require(\'smartsort\').sort',
-            build_sorted_words({ 1, 1 }, { '"c"', '"ss"', "require('smartsort').sort" })
+            build_sorted_words({ 1, 1 }, { '"c"', '"ss"', "require('smartsort').sort" }, ",")
         )
     end)
 end)
