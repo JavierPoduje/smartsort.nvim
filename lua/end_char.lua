582bdacda7875a0258b284a419cc03e16aaa200b
javierpoduje <javier.poduje@gmail.com>
2025-06-07 08:26:42 +0200

    fix: gaps usage

--- lua/end_char.lua
+++ lua/end_char.lua
@@ -7,9 +7,11 @@ local funcs = require("funcs")
 --- @field public is_attached boolean: true if the character is attached to the node
 --- @field public new fun(): EndChar
 ---
+--- @field public debug fun(self: EndChar): table
 --- @field public get_horizontal_gap fun(self: EndChar): number
 --- @field public get_vertical_gap fun(self: EndChar): number
 --- @field public new fun(char: string, gap: Gap, is_attached: boolean): EndChar
+--- @field public print fun(self: EndChar)
 --- @field public set_gaps fun(self: EndChar, endchar_cnode: Chadnode, other: Chadnode)
 --- @field public stringify fun(self: EndChar): string
 
@@ -31,6 +33,19 @@ function EndChar:new(char, gap, is_attached)
 end
 
 --- @param self EndChar
+--- @return table: a table representation of the EndChar for debugging
+EndChar.debug = function(self)
+    return {
+        char = self.char,
+        gap = {
+            horizontal_gap = self.gap.horizontal_gap,
+            vertical_gap = self.gap.vertical_gap
+        },
+        is_attached = self.is_attached,
+    }
+end
+
+--- @param self EndChar
 --- @return number: the horizontal gap
 EndChar.get_horizontal_gap = function(self)
     return self.gap.horizontal_gap or 0
@@ -40,6 +55,11 @@ end
 --- @return number: the vertical gap
 EndChar.get_vertical_gap = function(self)
     return self.gap.vertical_gap or 0
+end
+
+--- @param self EndChar
+EndChar.print = function(self)
+    print(vim.inspect(self:debug()))
 end
 
 --- @param self EndChar
@@ -55,10 +75,12 @@ end
 --- @param self EndChar
 --- @return string: the string representation of the endchar
 EndChar.stringify = function(self)
+    self:print()
     local gap_as_str = funcs.repeat_str(" ", self.gap.vertical_gap)
     if self.gap.vertical_gap == 0 and self.gap.horizontal_gap > 0 then
         gap_as_str = funcs.repeat_str(" ", self.gap.horizontal_gap)
     end
+
     return gap_as_str .. self.char
 end
 
