d08064856b64610203544615da8ae5bacea5f48d
javierpoduje <javier.poduje@gmail.com>
2025-06-07 18:55:01 +0200

    fix: typescript node_types

--- lua/treesitter/typescript/node_types.lua
+++ lua/treesitter/typescript/node_types.lua
@@ -1,13 +1,13 @@
-local merge_tables = require("funcs").merge_tables
+local funcs = require("funcs")
+local merge_arrays = funcs.merge_arrays
+local merge_tables = funcs.merge_tables
 local javascript_node_types = require("treesitter.javascript.node_types")
 
 local M = {}
 
 M.end_chars = merge_tables({}, javascript_node_types.end_chars)
-
-M.linkable = merge_tables({}, javascript_node_types.linkable)
-
-M.sortable = merge_tables({
+M.linkable = merge_arrays({}, javascript_node_types.linkable)
+M.sortable = merge_arrays({
     "interface_declaration",
     "pair",
     "property_signature",
