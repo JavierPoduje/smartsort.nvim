f97ddfba24bd681af2cbc2bdf73c8f5d43f5875c
javierpoduje <javier.poduje@gmail.com>
2025-07-26 15:00:25 +0200

    refactor: region tests

--- tests/chadnodes/from_region_spec.lua
+++ tests/chadnodes/from_region_spec.lua
@@ -17,13 +17,13 @@ describe("chadnodes: from_region", function()
         local cnodes = Chadnodes.from_region(bufnr, mock.region, parser)
 
         truthy(cnodes:__tostring(), [[
-const foo = () => {
-  console.log("foo");
-};
-// this is a comment
-function bar() {
-  console.log("bar");
-}
+            const foo = () => {
+              console.log("foo");
+            };
+            // this is a comment
+            function bar() {
+              console.log("bar");
+            }
         ]])
     end)
 
@@ -32,28 +32,19 @@ function bar() {
         local bufnr, parser = utils.setup(mock.content, "typescript")
         local cnodes = Chadnodes.from_region(bufnr, mock.region, parser)
 
-        truthy(vim.deep_equal(cnodes:debug(bufnr), {
-            {
-                ts_node = "/**\n * This is a comment\n */",
-                sort_key = ""
-            },
-            {
-                ts_node = 'const foo = () => {\n  console.log("foo");\n};',
-                sort_key = "foo"
-            },
-            {
-                ts_node = "// this is a comment",
-                sort_key = ""
-            },
-            {
-                ts_node = '// this comment "belongs" to the function',
-                sort_key = ""
-            },
-            {
-                ts_node = 'function bar() {\n  console.log("bar");\n}',
-                sort_key = "bar"
+        truthy(cnodes:__tostring(), [[
+            /**
+             * This is a comment
+             */
+            const foo = () => {
+              console.log("foo");
+            };
+            // this is a comment
+            // this comment "belongs" to the function
+            function bar() {
+              console.log("bar");
             }
-        }))
+        ]])
     end)
 
     it("shouldn't consider nodes outside region - start", function()
@@ -61,12 +52,11 @@ function bar() {
         local bufnr, parser = utils.setup(mock.content, "typescript")
         local cnodes = Chadnodes.from_region(bufnr, Region.new(1, 1, 3, 1), parser)
 
-        truthy(vim.deep_equal(cnodes:debug(bufnr), {
-            {
-                ts_node = 'const foo = () => {\n  console.log("foo");\n};',
-                sort_key = "foo"
-            },
-        }))
+        truthy(cnodes:__tostring(), [[
+            const foo = () => {
+              console.log("foo");
+            };
+        ]])
     end)
 
     it("shouldn't consider nodes outside region - end", function()
@@ -74,31 +64,19 @@ function bar() {
         local bufnr, parser = utils.setup(mock.content, "typescript")
         local cnodes = Chadnodes.from_region(bufnr, mock.region, parser)
 
-        truthy(vim.deep_equal(cnodes:debug(bufnr), {
-            {
-                ts_node = "// this is a comment",
-                sort_key = ""
-            },
-            {
-                ts_node = "// comment attached to the function zit",
-                sort_key = ""
-            },
-            {
-                ts_node = 'const zit = () => {\n  console.log("zit");\n};',
-                sort_key = "zit"
-            },
-            {
-                ts_node = "// nested comment",
-                sort_key = ""
-            },
-            {
-                ts_node = "/**\n * This is a comment\n */",
-                sort_key = ""
-            },
-            {
-                ts_node = 'function bar() {\n  console.log("bar");\n}',
-                sort_key = "bar"
+        truthy(cnodes:__tostring(), [[
+            // this is a comment
+            // comment attached to the function zit
+            const zit = () => {
+              console.log("zit");
+            };
+            // nested comment
+            /**
+             * This is a comment
+             */
+            function bar() {
+              console.log("bar");
             }
-        }))
+        ]])
     end)
 end)
