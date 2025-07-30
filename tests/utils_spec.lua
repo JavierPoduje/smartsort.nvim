6e856115a3322527ef14570419886835baeb7117
javierpoduje <javier.poduje@gmail.com>
2025-07-30 20:18:55 +0200

    refactor: test utils

--- tests/utils_spec.lua
+++ tests/utils_spec.lua
@@ -1,6 +1,7 @@
 local f = require("funcs")
-local parsers = require("nvim-treesitter.parsers")
 local typescript_mocks = require("tests.mocks.typescript")
+local setup = require("tests.utils").setup
+
 
 --- @diagnostic disable-next-line: undefined-global
 local describe = describe
@@ -9,40 +10,18 @@ local it = it
 --- @diagnostic disable-next-line: undefined-field
 local equal = assert.are.equal
 
---- @param buf_content string[]: the content of the buffer
---- @return number, vim.treesitter.LanguageTree
-local setup = function(buf_content)
-    vim.cmd(":new")
-    vim.api.nvim_buf_set_lines(0, 0, -1, false, buf_content)
-
-    local bufnr = vim.api.nvim_get_current_buf()
-    vim.api.nvim_win_set_cursor(0, { 1, 0 })
-
-    -- set filetype
-    local filetype = "typescript"
-    vim.bo[bufnr].filetype = filetype
-
-    -- set parser
-    local parser = parsers.get_parser(bufnr, filetype)
-    if not parser then
-        error("Parser not available for filetype: " .. filetype)
-    end
-    parser:parse()
-
-    return bufnr, parser
-end
 
 describe("utils", function()
     it("get_line_indent should recognize identation properly", function()
         local mock = typescript_mocks.simplest
-        local bufnr, _ = setup(mock.content)
+        local bufnr, _ = setup(mock.content, "typescript")
         local identation = f.get_line_indent(bufnr, 1)
         equal(identation, "  ")
     end)
 
     it("get_line_indent should recognize no identation properly", function()
         local mock = typescript_mocks.simplest
-        local bufnr, _ = setup(mock.content)
+        local bufnr, _ = setup(mock.content, "typescript")
         local identation = f.get_line_indent(bufnr, 0)
         equal(identation, "")
     end)
