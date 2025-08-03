3c8ddc7051547cb781c2d35338f896f6165dfcbf
javierpoduje <javier.poduje@gmail.com>
2025-08-03 12:25:09 +0200

    wip: update lua tests

--- tests/chadnodes/sort_lua_spec.lua
+++ tests/chadnodes/sort_lua_spec.lua
@@ -1,4 +1,5 @@
 local Chadnodes = require("chadnodes")
+local f = require("funcs")
 local lua_mocks = require("tests.mocks.lua")
 local utils = require("tests.utils")
 
@@ -9,17 +10,18 @@ local it = it
 --- @diagnostic disable-next-line: undefined-field
 local truthy = assert.is.truthy
 
-local default_setup = {
-    non_sortable_behavior = 'preserve',
-}
+local default_setup = utils.default_setup
 
 describe("chadnodes: sort - lua", function()
     it("should consider vertical and horizontal gaps properly", function()
         local mock = lua_mocks.third
         local bufnr, parser = utils.setup(mock.content, "lua")
-        local cnodes = Chadnodes.from_region(bufnr, mock.region, parser)
+        local cnodes, _, first_sortable_node_idx = Chadnodes.from_region(bufnr, mock.region, parser)
+        local opts = f.merge_tables(default_setup, {
+            first_sortable_node_idx = first_sortable_node_idx,
+        })
 
-        truthy(vim.deep_equal(cnodes:sort(default_setup):stringified_cnodes(), {
+        truthy(vim.deep_equal(cnodes:sort(opts):stringified_cnodes(), {
             "    aBoolean = true,",
             "    ,",
             '    aaa = "value of aaa",',
@@ -35,9 +37,12 @@ describe("chadnodes: sort - lua", function()
     it("should sort alphabetically", function()
         local mock = lua_mocks.simple
         local bufnr, parser = utils.setup(mock.content, "lua")
-        local cnodes = Chadnodes.from_region(bufnr, mock.region, parser)
+        local cnodes, _, first_sortable_node_idx = Chadnodes.from_region(bufnr, mock.region, parser)
+        local opts = f.merge_tables(default_setup, {
+            first_sortable_node_idx = first_sortable_node_idx,
+        })
 
-        truthy(vim.deep_equal(cnodes:sort(default_setup):stringified_cnodes(), {
+        truthy(vim.deep_equal(cnodes:sort(opts):stringified_cnodes(), {
             'M.a = function()\n    print("another guy called a")\nend',
             'M.b = function()\n    print("this is b")\nend',
         }))
@@ -46,9 +51,12 @@ describe("chadnodes: sort - lua", function()
     it("can sort variable declarations", function()
         local mock = lua_mocks.variables
         local bufnr, parser = utils.setup(mock.content, "lua")
-        local cnodes = Chadnodes.from_region(bufnr, mock.region, parser)
+        local cnodes, _, first_sortable_node_idx = Chadnodes.from_region(bufnr, mock.region, parser)
+        local opts = f.merge_tables(default_setup, {
+            first_sortable_node_idx = first_sortable_node_idx,
+        })
 
-        truthy(vim.deep_equal(cnodes:sort(default_setup):stringified_cnodes(), {
+        truthy(vim.deep_equal(cnodes:sort(opts):stringified_cnodes(), {
             'local varA = "something"',
             "--- @type boolean",
             "local varB = false",
