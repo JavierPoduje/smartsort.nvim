1df068904192cf9ee66fd636bc3430db9d8cd04b
javierpoduje <javier.poduje@gmail.com>
2025-08-09 13:02:27 +0200

    refactor: singleline sort stuff

--- lua/smartsort.lua
+++ lua/smartsort.lua
@@ -1,6 +1,5 @@
 local Chadnodes = require("chadnodes")
 local FileManager = require("file_manager")
-local R = require("ramda")
 local Region = require("region")
 local SinglelineSorter = require("singleline_sorter")
 local f = require("funcs")
@@ -76,46 +75,21 @@ M.region = function()
     print(Region.from_selection())
 end
 
---- Sort the selected line
---- @param region Region: the region to sort
---- @param args Args: the arguments to use
 M.sort_single_line = function(region, args)
-    if args.separator == nil then
-        print("Separator is required")
+    local raw_str = FileManager.get_line(region)
+
+    --- @type SinglelineSorter
+    local singleline_sorter = nil
+    local status, err = pcall(function()
+        singleline_sorter = SinglelineSorter.new(args.separator)
+    end)
+    if not status then
+        print(err)
         return
     end
 
-    assert(args.separator ~= nil, "Separator is required")
-
-    local separator = args.separator
-    local full_line = vim.fn.getline(region.srow, region.erow)
-    local raw_str = string.sub(full_line[1], region.scol, region.ecol)
-    local final_char_is_separator = false
-
-    local trimmed_str, leftpad, rightpad = M._trim(raw_str)
-    if string.sub(trimmed_str, -1) == separator then
-        trimmed_str = string.sub(trimmed_str, 1, -2)
-        final_char_is_separator = true
-    end
-    -- split words by separator, ignoring separators inside strings, and calculate spaces
-    local words, spaces_between_words = M._split_ignoring_strings_with_spaces(trimmed_str, separator)
-
-    table.sort(words)
-
-    local str_to_insert = table.concat({
-        leftpad,
-        M._build_sorted_words(spaces_between_words, words, separator),
-        final_char_is_separator and separator or "",
-        rightpad,
-    }, "")
-
-    FileManager.insert_line_in_buffer(region.srow, region.scol, region.ecol, str_to_insert)
-end
-
-M.sort_single_line_new_approach = function(region, args)
-    local raw_str = FileManager.get_line(region)
-    local sorted_line = SinglelineSorter.new(args.separator):sort(raw_str)
-    return sorted_line
+    local sorted_line = singleline_sorter:sort(raw_str)
+    FileManager.insert_line_in_buffer(region.srow, region.scol, region.ecol, sorted_line)
 end
 
 --- Sort the selected lines
@@ -157,161 +131,6 @@ M.sort_multiple_lines = function(selected_region, config)
         )
 
     FileManager.buf_set_lines(0, region.srow - 1, region.erow, sorted_nodes_with_gaps)
-end
-
---- Build the string of sorted words
---- @param spaces_between_words number[]: the spaces between words
---- @param words string[]: the words to build the sorted words of
---- @param separator string: the separator to use between words
---- @return string: the sorted words
-M._build_sorted_words = function(spaces_between_words, words, separator)
-    assert(
-        #words == #spaces_between_words + 1,
-        "Number of spaces between words must be one less than the number of words"
-    )
-
-    local output = R.reduce(function(acc, word, idx)
-        table.insert(acc, word)
-
-        -- add comma at the end of word if it's not the last word
-        if idx < #words then
-            table.insert(acc, separator)
-        end
-
-        -- add next gap if needed
-        if idx < #words then
-            local gap = string.rep(" ", spaces_between_words[idx])
-            table.insert(acc, gap)
-        end
-
-        return acc
-    end, {}, words)
-
-    return table.concat(output, "")
-end
-
---- Calculate the spaces between. The spaces are only calculated between words separated by a comma.
---- @param str string: the string to calculate the spaces between words of
---- @param separator string: the separator to use between words
---- @return number[]: the spaces between words
-M._calculate_spaces_between_words = function(str, separator)
-    if #str <= 1 then
-        return {}
-    end
-
-    --- @type number[]
-    local spaces = {}
-    local idx = 1
-    local count_spaces = false
-
-    while idx <= #str do
-        if string.sub(str, idx, idx) == separator and not M._is_inside_string(str, idx) then
-            idx = idx + 1
-            count_spaces = true
-        end
-
-        if count_spaces and string.sub(str, idx, idx) == " " then
-            local space_idx = idx
-            while string.sub(str, space_idx, space_idx) == " " do
-                space_idx = space_idx + 1
-            end
-            local number_of_spaces = space_idx - idx
-            table.insert(spaces, number_of_spaces)
-
-            idx = space_idx
-            count_spaces = false
-        elseif count_spaces and string.sub(str, idx, idx) ~= " " then
-            table.insert(spaces, 0)
-            idx = idx + 1
-            count_spaces = false
-        else
-            idx = idx + 1
-        end
-    end
-    return spaces
-end
-
---- Get the trimmed string, and the left and right "padding", where padding is the empty space
---- before and after the string.
----
---- @param str string: the string to get the padding of
---- @return string, string, string: the trimmed given string, the left padding as empty space, the right padding as empty space
-M._trim = function(str)
-    local leftpad = string.match(str, "^%s*")
-    local rightpad = string.match(str, "%s*$")
-    local trimmed_str = string.match(str, "^%s*(.-)%s*$")
-    return trimmed_str, leftpad, rightpad
-end
-
---- Split string by separator, ignoring separators inside string literals, and calculate spaces
---- @param str string: the string to split
---- @param separator string: the separator to split by
---- @return string[], number[]: array of split parts and spaces between them
-M._split_ignoring_strings_with_spaces = function(str, separator)
-    local parts = {}
-    local spaces = {}
-    local current_part = ""
-    local i = 1
-
-    while i <= #str do
-        local char = string.sub(str, i, i)
-
-        if char == separator and not M._is_inside_string(str, i) then
-            -- Found separator outside of string
-            table.insert(parts, current_part)
-            current_part = ""
-
-            -- Calculate spaces after this separator
-            local space_count = 0
-            i = i + 1
-            while i <= #str and string.sub(str, i, i) == " " do
-                space_count = space_count + 1
-                i = i + 1
-            end
-            table.insert(spaces, space_count)
-        else
-            current_part = current_part .. char
-            i = i + 1
-        end
-    end
-
-    -- Add the last part
-    table.insert(parts, current_part)
-
-    return parts, spaces
-end
-
---- Check if a character position is inside a string literal
---- @param str string: the string to check
---- @param pos number: the position in the string (1-indexed)
---- @return boolean: true if the position is inside a string literal
-M._is_inside_string = function(str, pos)
-    local i = 1
-    local in_string = false
-    local quote_char = nil
-
-    while i <= pos do
-        local char = string.sub(str, i, i)
-
-        if char == "'" or char == '"' then
-            if not in_string then
-                -- Start of string
-                in_string = true
-                quote_char = char
-            elseif quote_char == char then
-                -- End of string (only if it's the same quote type)
-                in_string = false
-                quote_char = nil
-            end
-        elseif char == "\\" and in_string then
-            -- Skip escaped character
-            i = i + 1
-        end
-
-        i = i + 1
-    end
-
-    return in_string
 end
 
 return M
--- tests/build_sorted_words_spec.lua
+++ tests/build_sorted_words_spec.lua
@@ -1,32 +1,44 @@
-local build_sorted_words = require('smartsort')._build_sorted_words
+local SinglelineSorter = require('singleline_sorter')
 
 --- @diagnostic disable-next-line: undefined-global
 local describe = describe
---- @diagnostic disable-next-line: undefined-global
-local it = it
 --- @diagnostic disable-next-line: undefined-field
 local equals = assert.are.equals
 --- @diagnostic disable-next-line: undefined-field
 local has_error = assert.has_error
+--- @diagnostic disable-next-line: undefined-global
+local it = it
 
 describe("smartsort._build_sorted_words", function()
     it("should error if 'spaces_between_words' has a length other than 'words' - 1", function()
-        has_error(function() build_sorted_words({ 1, 1 }, { "test", "deez" }, ",") end)
-        has_error(function() build_sorted_words({ 3 }, { "what", "about", "deez", "testies" }, ",") end)
-        has_error(function() build_sorted_words({ 1, 1, 1, 1, 1, }, { "this", "is", "a", "test" }, ",") end)
+        local slsorter = SinglelineSorter.new(",")
+
+        has_error(function()
+            slsorter:_build_sorted_words({ 1, 1 }, { "test", "deez" })
+        end)
+        has_error(function()
+            slsorter:_build_sorted_words({ 3 }, { "what", "about", "deez", "testies" })
+        end)
+        has_error(function()
+            slsorter:_build_sorted_words({ 1, 1, 1, 1, 1, }, { "this", "is", "a", "test" })
+        end)
     end)
 
     it("should respect spaces between words", function()
+        local slsorter = SinglelineSorter.new(",")
+
         equals(
             "this, is,  a,   test",
-            build_sorted_words({ 1, 2, 3 }, { "this", "is", "a", "test" }, ",")
+            slsorter:_build_sorted_words({ 1, 2, 3 }, { "this", "is", "a", "test" })
         )
     end)
 
     it("should respect non-alphanumeric characters", function()
+        local slsorter = SinglelineSorter.new(",")
+
         equals(
             '"c", "ss", require(\'smartsort\').sort',
-            build_sorted_words({ 1, 1 }, { '"c"', '"ss"', "require('smartsort').sort" }, ",")
+            slsorter:_build_sorted_words({ 1, 1 }, { '"c"', '"ss"', "require('smartsort').sort" })
         )
     end)
 end)
